01b1992e47a90022ddb30a8ebb08538bbd184eb4
Kate <katehilly1212@gmail.com>
2024-12-03 16:20:13 -0700

    Update index.js

--- lambda/index.js
+++ lambda/index.js
@@ -240,6 +240,6 @@ const skill = Alexa.SkillBuilders.custom()
     .create();
 const adapter = new ExpressAdapter(skill, false, false);
 const app = express();
-app.use('/', express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.post('/', adapter.getRequestHandlers());
 app.listen(3016);
